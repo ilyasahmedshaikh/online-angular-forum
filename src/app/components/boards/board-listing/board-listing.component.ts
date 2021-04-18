b9292bae8204be98d015da0ac793e10cd04a1029
ilyas ahmed <ilyasahmed081@gmail.com>
2021-04-18 18:22:57 +0500

    your boars screen done with routing added

--- src/app/components/boards/board-listing/board-listing.component.ts
+++ src/app/components/boards/board-listing/board-listing.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardListingComponent implements OnInit {
 
+  data: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.data = [
+      {
+        title: 'Lorem Ipsm',
+        date: '17-April-2021',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+      },
+      {
+        title: 'Lorem Ipsm',
+        date: '17-April-2021',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+      },
+      {
+        title: 'Lorem Ipsm',
+        date: '17-April-2021',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+      },
+      {
+        title: 'Lorem Ipsm',
+        date: '17-April-2021',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+      },
+      {
+        title: 'Lorem Ipsm',
+        date: '17-April-2021',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+      }
+    ]
   }
 
 }
